64c96121c1c4c17b3d8a2fe94132783babc4b7f1
Clayton Magalhães <claytonmagalhaes.dev@gmail.com>
2025-01-13 02:41:26 -0300

    refactor: :art: split main class into two classes
    The main class was refactored to improve code organization. The logic was divided into two separate classes: one for initial setup and the other for app execution, making the code easier to maintain and scale in the future.

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_arch_template/app.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Text('Flutter Demo Home'),
-    );
-  }
+  runApp(const App());
 }
