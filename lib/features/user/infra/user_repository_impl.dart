95488865d77ebd8704af4312b11150cfed672060
Clayton Magalhães <claytonmagalhaes.dev@gmail.com>
2025-01-26 15:01:53 -0300

    test: :white_check_mark: add tests for repository
    add tests for repository using mock for Dio implementation

--- lib/features/user/infra/user_repository_impl.dart
+++ lib/features/user/infra/user_repository_impl.dart
@@ -1,16 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_arch_template/core/common/failures.dart';
 import 'package:flutter_clean_arch_template/core/common/types/result.dart';
 import 'package:flutter_clean_arch_template/features/user/domain/user_entity.dart';
 import 'package:flutter_clean_arch_template/features/user/domain/user_repository.dart';
+import 'package:flutter_clean_arch_template/features/user/infra/user_model.dart';
 
 class UserRepositoryImpl implements UserRepository {
+  final Dio _dio;
+
+  const UserRepositoryImpl({required Dio dio}) : _dio = dio;
 
   @override
   Future<Result<List<UserEntity>>> getUsers() async {
-    // Simulate a network request
-    await Future.delayed(Duration(seconds: 1));
-    return Result.ok([
-      UserEntity(id: '1', name: 'John Doe', email: '  [email protected]'),
-      UserEntity(id: '2', name: 'Jane Doe', email: '  [email protected]'),
-    ]);
+    try {
+      final response =
+          await _dio.get('https://jsonplaceholder.typicode.com/users');
+      final users = (response.data as List)
+          .map((user) => UserModel.fromJson(user))
+          .toList();
+      final usersList = users.map((user) => user.toEntity()).toList();
+      return Result.ok(usersList);
+    } on DioException catch (e) {
+      return Result.error(NetworkFailure(
+        e.response?.data['error'] ?? 'An unexpected error occurred.',
+      ));
+    }
   }
 }
---
+++ test/features/user/infra/user_repository_impl_test.dart
@@ -0,0 +1,79 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_clean_arch_template/core/common/failures.dart';
+import 'package:flutter_clean_arch_template/core/common/types/result.dart';
+import 'package:flutter_clean_arch_template/features/user/infra/user_repository_impl.dart';
+import 'package:flutter_clean_arch_template/features/user/domain/user_entity.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockDio extends Mock implements Dio {}
+
+void main() {
+  late UserRepositoryImpl userRepository;
+  late MockDio mockDio;
+
+  setUp(() {
+    mockDio = MockDio();
+    userRepository = UserRepositoryImpl(dio: mockDio);
+  });
+
+  test('should return list of users when Dio call is successful', () async {
+    // arrange
+    final responseData = [
+      {"id": "1", "name": "Test User 1", "email": "[email protected]"},
+      {"id": "2", "name": "Test User 2", "email": "[email protected]"}
+    ];
+
+    //interceptor fake response
+    when(() => mockDio.get(any())).thenAnswer(
+      (_) async => Response(
+        data: responseData,
+        statusCode: 200,
+        requestOptions:
+            RequestOptions(path: 'https://jsonplaceholder.typicode.com/users'),
+      ),
+    );
+
+    // act
+    final result = await userRepository.getUsers();
+
+    // assert
+    expect(result, isA<Result<List<UserEntity>>>());
+    expect(result.value, isNotNull);
+    expect(
+      result.value,
+      [
+        UserEntity(id: '1', name: 'Test User 1', email: '[email protected]'),
+        UserEntity(id: '2', name: 'Test User 2', email: '[email protected]'),
+      ],
+    );
+    verify(() => mockDio.get('https://jsonplaceholder.typicode.com/users'))
+        .called(1);
+  });
+
+  test('should return NetworkFailure when Dio call fails', () async {
+    // arrange
+    when(() => mockDio.get(any())).thenThrow(
+      DioException(
+        requestOptions:
+            RequestOptions(path: 'https://jsonplaceholder.typicode.com/users'),
+        type: DioExceptionType.badResponse,
+        response: Response(
+          statusCode: 404,
+          data: {'error': 'Not Found'},
+          requestOptions: RequestOptions(
+              path: 'https://jsonplaceholder.typicode.com/users'),
+        ),
+      ),
+    );
+
+    // act
+    final result = await userRepository.getUsers();
+
+    // assert
+    expect(result, isA<Error<List<UserEntity>>>());
+    expect((result as Error<List<UserEntity>>).error, isA<NetworkFailure>());
+    verify(() => mockDio.get('https://jsonplaceholder.typicode.com/users'))
+        .called(1);
+  });
+}
