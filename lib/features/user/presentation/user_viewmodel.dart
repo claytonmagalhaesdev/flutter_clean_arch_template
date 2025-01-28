495635336ad556eafcad49614b423e9800488eea
Clayton Magalhães <claytonmagalhaes.dev@gmail.com>
2025-01-28 00:47:40 -0300

    refactor: :art: refactor viewmodel to use command pattern

--- lib/features/user/presentation/user_viewmodel.dart
+++ lib/features/user/presentation/user_viewmodel.dart
@@ -1,17 +1,23 @@
+import 'package:flutter_clean_arch_template/core/common/types/command.dart';
+import 'package:flutter_clean_arch_template/core/common/types/result.dart';
 import 'package:flutter_clean_arch_template/features/user/presentation/bloc/user_bloc.dart';
 import 'package:flutter_clean_arch_template/features/user/presentation/bloc/user_events.dart';
 import 'package:flutter_clean_arch_template/features/user/presentation/bloc/user_state.dart';
 
 class UserViewModel {
   final UserBloc userBloc;
+  late final Command0<void> fetchUsersCommand;
 
-  UserViewModel(this.userBloc);
+  UserViewModel(this.userBloc) {
+    fetchUsersCommand = Command0<void>(() async {
+      userBloc.add(FetchUsers());
+      return Result<void>.ok(null);
+    });
+  }
 
   // Expor estados
   Stream<UserState> get userStateStream => userBloc.stream;
 
-  // Expor eventos
-  void fetchUsers() {
-    userBloc.add(FetchUsers());
-  }
+  // Método para facilitar o uso na interface
+  Future<void> fetchUsers() => fetchUsersCommand.execute();
 }
