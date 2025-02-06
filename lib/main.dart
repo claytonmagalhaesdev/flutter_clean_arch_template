2a5090e14e3fd3e6369f5069996df5954f499994
Clayton Magalhães <claytonmagalhaes.dev@gmail.com>
2025-02-06 01:46:06 -0300

    refactor: :recycle: move to files correct

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_clean_arch_template/app.dart';
 import 'package:flutter_clean_arch_template/core/di/dependency_injector.dart';
 import 'package:flutter_clean_arch_template/core/di/get_it_dependency_injector.dart';
-import 'package:flutter_clean_arch_template/core/factories/app_module.dart';
+import 'package:flutter_clean_arch_template/core/app_module.dart';
 
 void main() {
   final DependencyInjector di = GetItDependencyInjector();
