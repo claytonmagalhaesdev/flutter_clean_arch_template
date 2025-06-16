a3e28feb99167d0a56e70bd1c76a84e9255720a1
Clayton Magalhães <claytonstradlin@gmail.com>
2025-06-16 15:28:46 -0300

    fix: :bug: fix mapper dto list

--- lib/core/common/infra/dto_mapper.dart
+++ lib/core/common/infra/dto_mapper.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_clean_arch_template/core/common/types/types.dart';
 
 abstract interface class DtoMapper<Dto> {
@@ -9,14 +8,19 @@ abstract interface class JsonMapper<Dto> {
   Json toJson(Dto dto);
 }
 
-abstract interface class Mapper<Dto> implements DtoMapper<Dto>, JsonMapper<Dto> {}
+abstract interface class Mapper<Dto>
+    implements DtoMapper<Dto>, JsonMapper<Dto> {}
 
 mixin DtoListMapper<Dto> implements DtoMapper<Dto> {
-  List<Dto> toDtoList(dynamic arr) => arr.map<Dto>(toDto).toList();
+  List<Dto> toDtoList(dynamic arr) => (arr as List<dynamic>)
+      .cast<Map<String, dynamic>>()
+      .map<Dto>((json) => toDto(json))
+      .toList();
 }
 
 mixin JsonArrMapper<Dto> implements JsonMapper<Dto> {
   JsonArr toJsonArr(List<Dto> list) => list.map(toJson).toList();
 }
 
-abstract base class ListMapper<Dto> with DtoListMapper<Dto>, JsonArrMapper<Dto> {}
+abstract base class ListMapper<Dto>
+    with DtoListMapper<Dto>, JsonArrMapper<Dto> {}
